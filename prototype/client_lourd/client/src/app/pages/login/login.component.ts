4401485eb26e8132639cbd316cad6c82e6202b6d
ArmanLidder <arman.lidder@polymtl.ca>
2024-09-05 01:30:06 -0400

    chatroom implemented + login fixes

--- prototype/client_lourd/client/src/app/pages/login/login.component.ts
+++ prototype/client_lourd/client/src/app/pages/login/login.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AuthService } from "../../services/auth.service";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from "@angular/material/snack-bar";
+import {SocketService} from "../../services/socket.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   authForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private snackBar: MatSnackBar) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private snackBar: MatSnackBar, private socketService: SocketService, private router: Router) {
     this.authForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+  ngOnInit(): void {
+    // Check if token exists and redirect to chatroom if already logged in
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['chatroom']);
+    }
   }
 
   login() {
@@ -24,16 +32,17 @@ export class LoginComponent {
       this.authService.login(loginData).subscribe(
         (res) => {
           this.snackBar.open("Connexion réussie", 'Fermer', {
-            duration: 5000,
+            duration: 2500,
             verticalPosition: 'bottom',
             horizontalPosition: 'center',
             panelClass : "my-snack-bar",
           });
+          this.router.navigate(['chatroom']);
         },
         (err) => {
           console.log(err)
           this.snackBar.open(err.error.msg, 'Fermer', {
-            duration: 5000,
+            duration: 2500,
             verticalPosition: 'bottom',
             horizontalPosition: 'center',
             panelClass : "my-snack-bar",
--- prototype/client_lourd/client/src/app/services/socket.service.ts
+++ prototype/client_lourd/client/src/app/services/socket.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
+  socket!: Socket;
 
-  constructor() { }
+  get() {
+    return this.socket;
+  }
+  connect(token: string) {
+    this.socket = io('http://localhost:8000', { transports: ['websocket'], upgrade: false, auth: {
+        token: token
+      }, });
+  }
+
+  isSocketAlive() {
+    return this.socket && this.socket.connected;
+  }
+
+  disconnect() {
+    this.socket.disconnect();
+  }
+
+  on<T>(event: string, action: (data: T) => void): void {
+    this.socket.on(event, action);
+  }
+
+  send<T, A>(event: string, data?: T, callback?: (data: A) => void): void {
+    this.socket.emit(event, ...[data, callback].filter((x) => x));
+  }
 }
