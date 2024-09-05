dca68a962f9c607049698f01ed20a93075fe5f26
Mehdi <mehdi.el-harami@polymtl.ca>
2024-09-05 23:00:47 +0100

    prototype client leger

--- prototype/client_leger/polyquiz/lib/main.dart
+++ prototype/client_leger/polyquiz/lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
-import 'pages/login-page.dart';
+import 'pages/auth_screen.dart';
+import 'pages/chat_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Polyquiz',
-      initialRoute: '/',
-      routes: {'/': (context) => LoginPage()},
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // Define the routes
+      routes: {
+        '/auth': (context) => AuthScreen(), // Define the route for authentication
+        '/chat': (context) => ChatScreen(), // Define the route for chat
+      },
+      initialRoute: '/auth', // Set the initial route
     );
   }
 }
