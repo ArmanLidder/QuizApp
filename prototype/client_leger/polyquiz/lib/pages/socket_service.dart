d82f008cbf3bff4fee6a62a09446bb92be308bc4
hb.96.montreal <hb.96.montreal@gmail.com>
2024-09-20 17:51:13 -0400

    fix: 2 differents connection on same client
    IO.Builder has to be force to generate new Options, that is why it was connecting to old account with old auth header instead of the newest one.
    
    Bug fixed + clean up unnecessary code

--- prototype/client_leger/polyquiz/lib/pages/socket_service.dart
+++ prototype/client_leger/polyquiz/lib/pages/socket_service.dart
@@ -1,41 +1,55 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
-  late IO.Socket socket;
+  static final SocketService _instance = SocketService._internal();
+  IO.Socket? socket;
 
-  SocketService getInstance() {
-    return SocketService();
+  SocketService._internal();
+
+  factory SocketService() {
+      return _instance;
   }
 
-  void connect(String token) async {
-    this.socket = IO.io(
+  void connect(String token) {
+    if (socket == null) {
+      socket = IO.io(
       'http://ec2-35-183-137-76.ca-central-1.compute.amazonaws.com:8000',
+      // "http://10.0.2.2:8000", // pour debug en localhost
       IO.OptionBuilder()
+        .enableForceNew()
         .setTransports(['websocket'])
-        .setAuth({'token': token})
         .disableAutoConnect()
+        .setAuth({'token': token})
         .build(),
-    );
+      );
+      
+      socket!.connect();
+          
+      socket!.onConnect((_) {
+          print('Connected to the socket server ${socket!.id}');
+      });
+          
+      socket?.onDisconnect((_) {
+          print('Disconnected to the socket server');
+      });
 
-    this.socket.connect();
-
-    socket.onConnect((_) {
-       print('Connected to the socket server ${socket.id}');
-    });
+    };
   }
-
+ 
   void sendMessage(String event, String message) {
-    if (this.socket.connected) {
-      this.socket.emit(event, message);
-
+    if (socket != null) {
+      socket!.emit(event, message);
     }
   }
 
   void on(String event, Function(dynamic) callback) {
-    this.socket.on(event, callback);
+    if (socket != null) {
+      socket!.on(event, callback);
+    }
   }
 
   void disconnect() {
-    this.socket.disconnect();
+    socket!.disconnect();
+    socket = null;
   }
 }
