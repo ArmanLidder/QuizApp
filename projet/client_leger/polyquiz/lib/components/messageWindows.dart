5e11075761c4f143d1d6d079504b4ac09d313332
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-06 22:52:04 -0400

    Ajout de la méthode sendMessage

--- projet/client_leger/polyquiz/lib/components/messageWindows.dart
+++ projet/client_leger/polyquiz/lib/components/messageWindows.dart
@@ -6,6 +6,20 @@ class MessageWindow extends StatefulWidget {
 }
 
 class _MessageWindowState extends State<MessageWindow> {
+  final TextEditingController _messageController = TextEditingController();
+  final List<String> _messages = [];
+
+  void _sendMessage() {
+    final message = _messageController.text.trim();
+    if (message.isEmpty) {
+      return;
+    }
+    setState(() {
+      _messages.add(message);
+    });
+    _messageController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
