621fd4e82f0eb375fd87b71ec7e7f9761e53426c
hb.96.montreal <hb.96.montreal@gmail.com>
2024-10-13 22:36:01 -0400

    Add Message and Canal interface
    interface for client lourd and client leger

--- projet/common/interfaces/message.interface.ts
+++ projet/common/interfaces/message.interface.ts
@@ -1,5 +1,20 @@
+// old will not use
+// export interface Message {
+//     sender: string;
+//     content: string;
+//     time: string;
+// }
+
 export interface Message {
-    sender: string;
-    content: string;
-    time: string;
+    userUid: string;
+    message: string;
+    createdAt?: any;
+}
+
+export interface Canal {
+    id?: string;
+    name: string;
+    isPrivate: boolean;
+    permittedUsers: string[]; // List of user.uid to check who can join
+    messages: Message[];
 }
