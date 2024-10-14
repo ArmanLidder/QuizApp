e2bdf2362068b6d2602117a93c55659733409e9e
hb.96.montreal <hb.96.montreal@gmail.com>
2024-10-13 22:36:33 -0400

    Server time created messages

--- projet/client_lourd/src/app/services/canal.service/canal.service.ts
+++ projet/client_lourd/src/app/services/canal.service/canal.service.ts
@@ -1,21 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable, firstValueFrom } from 'rxjs';
+import { updateDoc, arrayUnion } from '@angular/fire/firestore';
+import {Observable, firstValueFrom} from 'rxjs';
 import { map } from 'rxjs/operators';
-
-export interface Message {
-  userUid: string;
-  message: string;
-  createdAt: string;
-}
-
-export interface Canal {
-  id?: string;
-  name: string;
-  isPrivate: boolean;
-  permittedUsers: string[]; // List of user.uid to check who can join
-  messages: Message[];
-}
+import {ServerTimeService} from "@app/services/server-time.service/server-time.service";
+import { Message, Canal } from "@common/interfaces/message.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -25,7 +14,7 @@ export class CanalService {
   private canalsCollection: AngularFirestoreCollection<Canal>;
   canals$: Observable<Canal[]>; // Keep a list of all canals at all time
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private serverTimeService: ServerTimeService) {
     this.canalsCollection = this.firestore.collection<Canal>('canals');
     this.canals$ = this.canalsCollection.snapshotChanges().pipe(
         map(actions => actions.map(a => {
@@ -48,7 +37,6 @@ export class CanalService {
     canals.map((canal: Canal) => {
       canal_names.push(canal.name);
     });
-    canal_names.forEach((name: string) => console.log(name));
     if (canal_names.includes(name)) throw new Error("Le nom de canal est déjà utiliser");
     const newCanal: Canal = {
       name,
@@ -76,13 +64,14 @@ export class CanalService {
   }
 
   async addMessage(canalId: string, message: Message): Promise<void> {
-    const canalRef = this.canalsCollection.doc(canalId);
-    return this.firestore.firestore.runTransaction(async (transaction) => {
-      const canalDoc = await transaction.get(canalRef.ref);
-      if (!canalDoc.exists) throw new Error("Le canal n'existe pas");
-      const canalData = canalDoc.data() as Canal;
-      const updatedMessages = [...canalData.messages, message];
-      transaction.update(canalRef.ref, { messages: updatedMessages });
+    const date: Date = await this.serverTimeService.getServerTime();
+    const messageWithTimestamp: Message = {
+      ...message,
+      createdAt: date // Use serverTimestamp here
+    };
+    const canalRef = this.canalsCollection.doc(canalId).ref;
+    await updateDoc(canalRef, {
+      messages: arrayUnion(messageWithTimestamp)
     });
   }
 }
