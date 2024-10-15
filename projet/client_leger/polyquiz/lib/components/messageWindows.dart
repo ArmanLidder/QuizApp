dc7c2ca5bd68551035130799ffd095e8ab4aca4f
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-14 21:47:39 -0400

    Message block gris avec les coins ronds

--- projet/client_leger/polyquiz/lib/components/messageWindows.dart
+++ projet/client_leger/polyquiz/lib/components/messageWindows.dart
@@ -23,62 +23,57 @@ class _MessageWindowState extends State<MessageWindow> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: <Widget>[
-          // Cette section est la liste des messages déjà envoyés
-          Expanded(
-              child: ListView.builder(
-                  itemCount: _messages.length,
-                  itemBuilder: (context, index) {
-                    final message = _messages[index];
-                    return buildMessageBlock(message, "me");
-                  }
-              )
+        child: Column(children: <Widget>[
+      // Cette section est la liste des messages déjà envoyés
+      Expanded(
+          child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return buildMessageBlock(message, "me");
+              })),
+      Container(
+          child: Row(children: <Widget>[
+        Expanded(
+            child: TextField(
+          controller: _messageController,
+          decoration: InputDecoration(
+            hintText: "input a message...",
+            border: InputBorder.none,
           ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _messageController,
-                    decoration: InputDecoration(
-                      hintText: "input a message...",
-                      border: InputBorder.none,
-                    ),
-                    onSubmitted: (value) => _sendMessage(),
-                  )
-                ),
-                IconButton(onPressed: _sendMessage, icon: Icon(Icons.send, color: Colors.blue,))
-              ]
-            )
-          )
-        ]
-      )
-    );
+          onSubmitted: (value) => _sendMessage(),
+        )),
+        IconButton(
+            onPressed: _sendMessage,
+            icon: Icon(
+              Icons.send,
+              color: Colors.blue,
+            ))
+      ]))
+    ]));
   }
 
   Widget buildMessageBlock(String content, String author) {
     return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          Text(
-            author,
-            style: TextStyle(
-              fontSize: 12.0,
-              fontWeight: FontWeight.bold
+        padding: EdgeInsets.all(18.0),
+        child: Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(12.0)
             ),
-          ),
-          // Il faudrait avoir un espace
-          Text(
-            content,
-            style: TextStyle(
-              fontSize: 16.0,
-            )
-          ),
-        ]
-      )
-    );
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              Text(
+                author,
+                style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
+              ),
+              // Il faudrait avoir un espace
+              Text(content,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  )),
+            ])));
   }
 }
