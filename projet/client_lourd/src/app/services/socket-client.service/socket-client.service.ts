820d0e1f8ef12a3ad4bed0e62803594d82ea555e
hb.96.montreal <hb.96.montreal@gmail.com>
2024-10-17 13:59:43 -0400

    Binding firebase UserID to user socket
    To ensure that we can collect data from firebase directly from server

--- projet/client_lourd/src/app/services/socket-client.service/socket-client.service.ts
+++ projet/client_lourd/src/app/services/socket-client.service/socket-client.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
+import { UsersService } from "@app/services/users.service/users.service";
+import {firstValueFrom} from "rxjs";
+import {User} from "@common/interfaces/user-data.interface";
 
 @Injectable({
     providedIn: 'root',
 })
 export class SocketClientService {
+    private userID: string;
     socket: Socket;
+
+    constructor(private usersService: UsersService) {}
 
     isSocketAlive() {
         return this.socket && this.socket.connected;
     }
 
     connect() {
-        const serverUrlWithoutApi = environment.serverUrl.replace('/api', '');
-        console.log(serverUrlWithoutApi)
-        this.socket = io(serverUrlWithoutApi, {
-            transports: ['websocket'],
-            upgrade: false,
-            auth: {
-                token: localStorage.getItem('token'),
-            }
+        this.fetchUserID().then(() => {
+            console.log('Connection du socket client')
+            const serverUrlWithoutApi = environment.serverUrl.replace('/api', '');
+            console.log(`UserId: ${this.userID}`)
+            this.socket = io(serverUrlWithoutApi, {
+                transports: ['websocket'],
+                upgrade: false,
+                auth: {
+                    userId: this.userID
+                },
+            });
         });
     }
 
@@ -35,5 +44,12 @@ export class SocketClientService {
 
     send<T, A>(event: string, data?: T, callback?: (data: A) => void): void {
         this.socket.emit(event, ...[data, callback].filter((x) => x));
+    }
+
+    private async fetchUserID() {
+        const user: User | null = await firstValueFrom(this.usersService.currentUserProfile$)
+        console.log(`Fetch userId: ${user?.uid}`)
+        if (user) this.userID = user.uid;
+        else throw new Error("User Id not fetch since user is null or undefined");
     }
 }
