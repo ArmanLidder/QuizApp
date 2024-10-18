14a2c00dc6a49e7c664803611a8d82b7ac52487f
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-18 15:00:16 -0400

    Base for the chat widget is in place

--- projet/client_leger/polyquiz/lib/widgets/chat_widgets/chat_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/chat_widget.dart
@@ -7,6 +7,25 @@ class ChatWidget extends StatefulWidget {
 class _ChatWidgetState extends State<ChatWidget> {
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World");
+    return Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+                color: Colors.blue,
+                child: Row(
+                  children: [
+                    Icon(Icons.textsms, size: 40),
+                    Text("Clavardage", style: TextStyle(fontSize: 40)),
+                  ],
+                )
+            ),
+          ),
+          Expanded(
+            flex: 9,
+              child: Container(
+                  child: Text('To be filled'),
+          ))
+        ]
+    );
   }
 }
