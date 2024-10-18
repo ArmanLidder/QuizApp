1c4dfde9028c9917ee8ef2699cdc5cd0dd8e6913
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-18 14:35:09 -0400

    Boilerplate for ChatWidget

--- projet/client_leger/polyquiz/lib/widgets/chat_widgets/chat_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/chat_widget.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+class ChatWidget extends StatefulWidget {
+  State<ChatWidget> createState() => _ChatWidgetState();
+}
+
+class _ChatWidgetState extends State<ChatWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello World");
+  }
+}
