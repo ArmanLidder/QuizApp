a08700127cacdad87d14e720ed99300bea74367a
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-18 15:04:10 -0400

    Boilerplate for channel window

---
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/channel_window_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChannelWindowWidget extends StatefulWidget {
+  State<ChannelWindowWidget> createState() => _ChannelWindowWidgetState();
+}
+
+class _ChannelWindowWidgetState extends State<ChannelWindowWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('not yet implemented');
+  }
+}
