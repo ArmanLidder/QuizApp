afe1ac4e8309d88c5e2b29843a676977b821edc8
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-18 15:31:44 -0400

    Boilerplate for channel selection window

--- projet/client_leger/polyquiz/lib/widgets/chat_widgets/channel_window_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/channel_window_widget.dart
@@ -10,3 +10,14 @@ class _ChannelWindowWidgetState extends State<ChannelWindowWidget> {
     return Text('not yet implemented');
   }
 }
+
+class ChannelSelectionWidget extends StatefulWidget {
+  State<ChannelSelectionWidget> createState() => _ChannelSelectionWidgetState();
+}
+
+class _ChannelSelectionWidgetState extends State<ChannelSelectionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('not yet implemented');
+  }
+}
