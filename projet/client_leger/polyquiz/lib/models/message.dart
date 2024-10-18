3cc064eedae7475c26e5af9e57025cc773b4ec35
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-18 13:54:08 -0400

    Added channel interface to projet for light client

--- projet/client_leger/polyquiz/lib/models/message.dart
+++ projet/client_leger/polyquiz/lib/models/message.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 class Message {
   final String userUid;
   final String message;
@@ -14,5 +16,34 @@ class Message {
     'userUid': userUid,
     'message': message,
     'createdAt': createdAt,
+  };
+}
+
+class Canal {
+  final String? id;
+  final String name;
+  final Bool isPrivate;
+  final List<String> permittedUsers;
+  final List<Message> messages;
+
+  Canal(this.name, this.isPrivate, this.permittedUsers, this.messages, {this.id});
+
+  Canal.fromJson(Map<String, dynamic> json)
+    : id = json['id'] != null ? json['id'] as String : null,
+      name = json['name'] as String,
+      isPrivate = json['isPrivate'] as Bool,
+      permittedUsers = (json['permittedUsers'] as List<dynamic>)
+        .map((user) => user as String)
+        .toList(),
+      messages = (json['messages'] as List<dynamic>)
+        .map((message) => Message.fromJson(message))
+        .toList();
+
+  Map<String, dynamic> toJson() => {
+    'id': id != null ? id : null,
+    'name': name,
+    'isPrivate': isPrivate,
+    'permittedUsers': permittedUsers,
+    'messages': messages.map((message) => message.toJson()).toList()
   };
 }
