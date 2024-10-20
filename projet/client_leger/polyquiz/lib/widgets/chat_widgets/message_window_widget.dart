ac064f47c54a393b144dd3f92e78a03b7fb60d33
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-19 22:38:04 -0400

    Created basic model for message window

---
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/message_window_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:polyquiz/models/message.dart';
+
+class MessageWindowWidget extends StatefulWidget {
+  final void Function() returnCallback;
+  final String channelId;
+  const MessageWindowWidget({required this.channelId, required this.returnCallback, super.key});
+
+  @override
+  State<MessageWindowWidget> createState() => _MessageWindowWidgetState();
+}
+
+class _MessageWindowWidgetState extends State<MessageWindowWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            IconButton(onPressed: (){ widget.returnCallback(); }, icon: Icon(Icons.arrow_back)),
+            Text("Channel Name") // TODO: Change once I can access channel name
+          ]
+        ),
+      ]
+    );
+  }
+}
+
+class MessageListWidget extends StatelessWidget {
+  final List<Message> messages;
+  const MessageListWidget({required this.messages, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: messages.length,
+        itemBuilder: (context, index) {
+          return MessageTile(content: messages[index].message, userId: messages[index].userUid);
+        }
+    );
+  }
+}
+
+class MessageTile extends StatelessWidget {
+  final String content;
+  final String userId;
+  const MessageTile({required this.content, required this.userId, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(content);
+  }
+}
