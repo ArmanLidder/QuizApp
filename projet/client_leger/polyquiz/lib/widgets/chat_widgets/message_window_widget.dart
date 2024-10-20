184c80d435b436405a508ba40a3a8cd15b4ad1bf
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-20 01:28:11 -0400

    Added styling to messages

--- projet/client_leger/polyquiz/lib/widgets/chat_widgets/message_window_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/message_window_widget.dart
@@ -21,20 +21,20 @@ class _MessageWindowWidgetState extends State<MessageWindowWidget> {
       children: <Widget>[
         Row(
           children: <Widget>[
-            IconButton(onPressed: (){ widget.returnCallback(); }, icon: Icon(Icons.arrow_back)),
-            Text("Channel Name") // TODO: Change once I can access channel name
+            Expanded(child: IconButton(onPressed: (){ widget.returnCallback(); }, icon: Icon(Icons.arrow_back))),
+            Expanded(child: Text(getChannel().name)) // TODO: Change once I can access channel name
           ]
         ),
         Expanded(child: Obx(() {
-          return MessageListWidget(messages: getMessagesFromChannel().isEmpty ? [] : getMessagesFromChannel());
+          return MessageListWidget(messages: getChannel().messages.isEmpty ? [] : getChannel().messages);
         }))
       ]
     );
   }
 
-  List<Message> getMessagesFromChannel() {
+  Canal getChannel() {
     bool channelById(Canal channel) => channel.id == widget.channelId;
-    return channelService.channels.toList().firstWhere(channelById).messages;
+    return channelService.channels.toList().firstWhere(channelById);
   }
 }
 
@@ -54,12 +54,63 @@ class MessageListWidget extends StatelessWidget {
 }
 
 class MessageTile extends StatelessWidget {
+  final imageUrl = "https://i.pinimg.com/originals/87/a2/d6/87a2d6017b9a7cc38274cef92a45cee3.jpg"; // TODO: Remove and add images
+  final username = "Elsa";
+
   final String content;
   final String userId;
   const MessageTile({required this.content, required this.userId, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(content);
+    return buildSentMessage();
+  }
+
+  Widget buildSentMessage() {
+    return ListTile(
+      title: Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(content)
+        ),
+      ),
+      trailing: buildUserInfo(),
+    );
+  }
+
+  Widget buildReceivedMessage() {
+    return ListTile(
+      title: Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text(content)
+        ),
+      ),
+      leading: buildUserInfo(),
+    );
+  }
+
+  Widget buildUserInfo() {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(imageUrl),
+          ),
+        ),
+        Flexible(child: Text(username))
+      ],
+    );
   }
 }
