eda6a99cf89961c591d3dce2971ea5410542adbd
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-19 22:38:23 -0400

    Made page responsive and selection of channels available

--- projet/client_leger/polyquiz/lib/widgets/chat_widgets/chat_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/chat_widget.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:polyquiz/widgets/chat_widgets/channel_window_widget.dart';
+import 'package:polyquiz/widgets/chat_widgets/message_window_widget.dart';
+
+enum Page { Channel, Message }
 
 class ChatWidget extends StatefulWidget {
   State<ChatWidget> createState() => _ChatWidgetState();
 }
 
 class _ChatWidgetState extends State<ChatWidget> {
+  Page page = Page.Channel;
   String title = "Clavardage";
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -25,15 +30,30 @@ class _ChatWidgetState extends State<ChatWidget> {
           Expanded(
             flex: 9,
               child: Container(
-                  child: ChannelWindowWidget(selectChannel),
+                  child: buildCurrentPage(),
           ))
         ]
     );
   }
 
+  Widget buildCurrentPage() {
+    switch (page) {
+      case Page.Message:
+        return MessageWindowWidget(channelId: 'channelId', returnCallback: (){
+          setState(() {
+            page = Page.Channel;
+          });
+        });
+      case Page.Channel:
+      default:
+        return ChannelWindowWidget(selectChannel);
+    }
+  }
+
   void selectChannel(String name) {
     setState(() {
       title = name;
+      page = Page.Message;
     });
   }
 }
