83bcf954eb8667fa1f0bcb4e0f3eecc305209080
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-19 21:23:31 -0400

    Added firebase initializing and await

--- projet/client_leger/polyquiz/lib/main.dart
+++ projet/client_leger/polyquiz/lib/main.dart
@@ -7,9 +7,9 @@ import 'pages/join_room_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
