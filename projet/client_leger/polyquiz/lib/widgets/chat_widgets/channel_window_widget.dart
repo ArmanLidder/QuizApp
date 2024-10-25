ec25b3e97df4b5391e26a32f2575262dfb0e02e3
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-10-25 17:51:10 -0400

    Added join channel window

--- projet/client_leger/polyquiz/lib/widgets/chat_widgets/channel_window_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/chat_widgets/channel_window_widget.dart
@@ -4,6 +4,11 @@ import 'package:polyquiz/models/message.dart';
 import 'package:polyquiz/services/channelService.dart';
 
 // to be removed once connected to database
+enum Page {
+  select,
+  join,
+  create,
+}
 
 class ChannelWindowWidget extends StatefulWidget {
   final void Function(String) updateCurrentChannel;
@@ -13,16 +18,35 @@ class ChannelWindowWidget extends StatefulWidget {
 }
 
 class _ChannelWindowWidgetState extends State<ChannelWindowWidget> {
+  Page currentPage = Page.select;
+
   @override
   Widget build(BuildContext context) {
-    return ChannelSelectionWidget(widget.updateCurrentChannel);
+    return buildCurrentPage();
+  }
+
+  Widget buildCurrentPage() {
+    switch (currentPage) {
+      case Page.join:
+        return ChannelJoiningWidget(returnCallback: () {selectChannel(Page.select);});
+      case Page.select:
+      default:
+        return ChannelSelectionWidget(widget.updateCurrentChannel, selectChannel);
+    }
+  }
+
+  void selectChannel(Page value) {
+    setState(() {
+      currentPage = value;
+    });
   }
 }
 
 class ChannelSelectionWidget extends StatefulWidget {
   final void Function(String) updateCurrentChannel;
+  final void Function(Page) changePage;
 
-  ChannelSelectionWidget(this.updateCurrentChannel);
+  ChannelSelectionWidget(this.updateCurrentChannel, this.changePage);
 
   State<ChannelSelectionWidget> createState() => _ChannelSelectionWidgetState();
 }
@@ -42,14 +66,14 @@ class _ChannelSelectionWidgetState extends State<ChannelSelectionWidget> {
         Row(
           children: <Widget>[
             Expanded(child: ElevatedButton(onPressed: (){}, child: Text("Créer un canal"))),
-            Expanded(child: ElevatedButton(onPressed: (){}, child: Text("Joindre un canal")))
+            Expanded(child: ElevatedButton(onPressed: (){widget.changePage(Page.join);}, child: Text("Joindre un canal")))
           ]
         ),
         Expanded(child: Obx(() { return channelService.channels.isEmpty ? Text("empty") :
           ListView.builder(
-            itemCount: channelService.channels.length,
+            itemCount: channelService.permittedChannels.length,
               itemBuilder: (context, index) {
-                return ChannelSelectionButton(buttonCallback: widget.updateCurrentChannel, name: channelService.channels[index].name, id: channelService.channels[index].id ?? "null");
+                return ChannelSelectionButton(buttonCallback: widget.updateCurrentChannel, name: channelService.permittedChannels[index].name, id: channelService.channels[index].id ?? "null");
               }
         );}))
       ]
@@ -88,6 +112,67 @@ class ChannelSelectionButton extends StatelessWidget {
             IconButton(onPressed: (){}, icon: Icon(Icons.delete))
           ]
         ),
+      )
+    );
+  }
+}
+
+class ChannelJoiningWidget extends StatefulWidget {
+  final void Function() returnCallback;
+  const ChannelJoiningWidget({required this.returnCallback, super.key});
+
+  @override
+  State<ChannelJoiningWidget> createState() => _ChannelJoiningWidgetState();
+}
+
+class _ChannelJoiningWidgetState extends State<ChannelJoiningWidget> {
+  final channelService = Get.put(ChannelService());
+  String _query = "";
+
+  void searchChannels(String query) {
+    setState(() {
+      _query = query;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+            children: <Widget>[
+              Expanded(child: Align(alignment: Alignment.centerLeft, child: IconButton(onPressed: (){ widget.returnCallback(); }, icon: Icon(Icons.arrow_back)))),
+              Expanded(child: Align(alignment: Alignment.center, child: Text("Joindre Canal")))
+            ]
+        ),
+        // buildSearchBar(),
+        Expanded(
+          child: Obx(() => ListView.builder(
+                itemCount: channelService.joinableChannels.length,
+                itemBuilder: (context, index) {
+                  final channel = channelService.joinableChannels[index];
+                  return buildChannelTile(channel.name, channel.id ?? '');
+                }
+            ),
+          ),
+        )
+      ]
+    );
+  }
+
+  Widget buildSearchBar() {
+    return Placeholder();
+  }
+
+  Widget buildChannelTile(String name, String id) {
+    return ListTile(
+        title: TextButton(
+          onPressed: () {},
+          child: Container(child: Text(name)),
+          style: TextButton.styleFrom(
+            alignment: Alignment.centerLeft,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero)
+          )
       )
     );
   }
