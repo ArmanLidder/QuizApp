9b90709494fc771a8c42be45e4bb600e138af9d2
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-11-01 23:26:39 -0400

    added more methods to notification service

--- projet/client_leger/polyquiz/lib/services/notification_service.dart
+++ projet/client_leger/polyquiz/lib/services/notification_service.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:polyquiz/models/message.dart';
 import 'package:polyquiz/services/channelService.dart';
 
 class NotificationService extends GetxController {
@@ -7,6 +9,39 @@ class NotificationService extends GetxController {
   Map<String, int> channelMessageCount = {};
   Map<String, bool> isChannelRead = {};
 
+  bool isChannelPermitted(String channelId) => channelService.getListOfPermittedChannelIds().contains(channelId);
 
+  void handlePermittedChannel(Canal channel) {
+    if (channel.id == null) return;
+    if (!channelMessageCount.containsKey(channel.id)) {
+      channelMessageCount[channel.id!] = channel.messages.length;
+      isChannelRead[channel.id!] = true;
+      return;
+    }
+
+    if (channelMessageCount[channel.id] == null) return;
+
+    if (channel.messages.length > channelMessageCount[channel.id]!) {
+      channelMessageCount[channel.id!] = channel.messages.length;
+      isChannelRead[channel.id!] = false;
+    }
+  }
+
+  void handleUnpermittedChannel(Canal channel) {
+    if (channelMessageCount.containsKey(channel.id)) channelMessageCount.remove(channel.id);
+    if (isChannelRead.containsKey(channel.id)) isChannelRead.remove(channel.id);
+  }
+
+  void setUpChannelListener() {
+    channelService.getChannelStream().listen((snapshot) {
+      snapshot.docChanges.forEach((element) {
+        Canal changedChannel = Canal.fromDocument(element.doc);
+        if (changedChannel.id == null) return;
+
+        if (isChannelPermitted(changedChannel.id!)) handlePermittedChannel(changedChannel);
+        else handleUnpermittedChannel(changedChannel);
+      });
+    });
+  }
 
 }
