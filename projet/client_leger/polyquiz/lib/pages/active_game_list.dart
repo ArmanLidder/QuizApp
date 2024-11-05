b4ea431881246f67d87e71e9637aeb4f07c19464
Mehdi <mehdi.el-harami@polymtl.ca>
2024-11-05 03:28:39 +0100

    finish list of games actif

--- projet/client_leger/polyquiz/lib/pages/active_game_list.dart
+++ projet/client_leger/polyquiz/lib/pages/active_game_list.dart
@@ -8,17 +8,13 @@ import 'package:polyquiz/services/waiting_room_service.dart';
 import 'package:polyquiz/services/user_service.dart';
 import 'package:polyquiz/models/game_list_item.dart';
 import 'package:polyquiz/models/user.dart';
+import '../services/waiting_room_service.dart';
+import 'waiting_room_screen.dart';
+import '../models/quiz.dart';
+
 
 class ActiveGameListComponent extends StatefulWidget {
-  final Function(int) sendRoomData;
-  final Function(String) sendUsernameData;
-  final Function(bool) validationDone;
-
-  ActiveGameListComponent({
-    required this.sendRoomData,
-    required this.sendUsernameData,
-    required this.validationDone,
-  });
+  ActiveGameListComponent({Key? key}) : super(key: key);
 
   @override
   _ActiveGameListComponentState createState() => _ActiveGameListComponentState();
@@ -28,6 +24,8 @@ class _ActiveGameListComponentState extends State<ActiveGameListComponent> {
   late Future<void> _initializeFuture;
   Map<String, String> quizNameMap = {};
   WaitingRoomService waitingRoomService = WaitingRoomService();
+  final UserService userService = UserService();
+  bool _isJoining = false;
 
   @override
   void initState() {
@@ -60,6 +58,42 @@ class _ActiveGameListComponentState extends State<ActiveGameListComponent> {
     });
   }
 
+  Future<void> _joinWaitingRoom(String roomID, String username) async {
+
+      setState(() {
+        _isJoining = true;
+      });
+
+      try {
+        // Navigate to the WaitingRoomScreen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => WaitingRoomScreen(
+              quiz: Quiz(
+                id: roomID, // Pass the room ID to the waiting room.
+                title: 'Nothing', // Provide a sample title.
+                description: 'Nothing', // Provide a sample description.
+                duration: 0, // Provide a sample duration.
+                questions: [], // Provide an empty list of questions.
+              ),
+              username: username, // Pass the username to the waiting room.
+              isHost: false, // This user is not the host.
+            ),
+          ),
+        );
+      } catch (e) {
+        setState(() {
+          _isJoining = false;
+        });
+
+        // Display an error message if joining fails.
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to join room: $e')),
+        );
+      }
+  }
+
   String _minimumPrestige(int prestige) {
     if (prestige >= 200) return '🏅'; // Platinum medal
     if (prestige >= 150) return '🥇'; // Gold medal
@@ -71,7 +105,6 @@ class _ActiveGameListComponentState extends State<ActiveGameListComponent> {
   Future<void> _joinRoom(GameListItem game) async {
     final roomValidationService = Provider.of<RoomValidationService>(context, listen: false);
     final snackbarService = Provider.of<SnackbarService>(context, listen: false);
-
     roomValidationService.roomId = game.room.toString();
     final isHostFriend = await _validateFriendship(game);
     if (game.friendsOnly && !isHostFriend) {
@@ -85,12 +118,30 @@ class _ActiveGameListComponentState extends State<ActiveGameListComponent> {
         if (!roomValidationService.isUsernameValid) {
           snackbarService.show("Vous avez été banni de cette partie.");
         } else {
-          await waitingRoomService.connectToSocket(game.room.toString(), isHost: false, username: roomValidationService.userData!.username);
           if (roomValidationService.isLocked) {
             snackbarService.show("La partie est actuellement verouillez.");
           }
+          // _joinWaitingRoom(game.room.toString(), roomValidationService.userData!.username);
           final isValid = !roomValidationService.isLocked && roomValidationService.isUsernameValid;
-          if (isValid) _sendAllDataToWaitingRoom();
+          if (true) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => WaitingRoomScreen(
+                  quiz: Quiz(
+                    id: roomValidationService.roomId!, // Pass the room ID to the waiting room.
+                    title: 'Nothing', // Provide a sample title.
+                    description: 'Nothing', // Provide a sample description.
+                    duration: 0, // Provide a sample duration.
+                    questions: [], // Provide an empty list of questions.
+                  ),
+                  username: roomValidationService.userData!.uid, // Pass the username to the waiting room.
+                  isHost: false, // This user is not the host.
+                  isFromActiveList: true,
+                ),
+              ),
+            );
+          }
         }
       }
     }
@@ -98,72 +149,97 @@ class _ActiveGameListComponentState extends State<ActiveGameListComponent> {
 
   Future<bool> _validateFriendship(GameListItem game) async {
     final roomValidationService = Provider.of<RoomValidationService>(context, listen: false);
-    final userService = Provider.of<UserService>(context, listen: false);
-
-    final currentUserId = (await roomValidationService.user$.first)?.uid;
-    final hostProfile = await userService.getUserById(game.hostUserId);
+    final currentUserId = roomValidationService.userData!.username;
+    final hostProfile = await this.userService.getUserById(game.hostUserId);
     return hostProfile?.friends.contains(currentUserId) ?? false;
   }
 
   Future<bool> _validatePrestige(int prestige) async {
     final roomValidationService = Provider.of<RoomValidationService>(context, listen: false);
 
-    final currentUserPrestige = (await roomValidationService.user$.first)?.prestige;
-    return currentUserPrestige != null && currentUserPrestige >= prestige;
+    final currentUserPrestige = roomValidationService.userData!.prestige;
+    return currentUserPrestige >= prestige;
   }
 
   void _sendAllDataToWaitingRoom() {
     final roomValidationService = Provider.of<RoomValidationService>(context, listen: false);
 
     if (roomValidationService.roomId != null) {
-      widget.sendRoomData(int.parse(roomValidationService.roomId!));
-      widget.sendUsernameData(roomValidationService.username!);
+      roomValidationService.isActive = false;
+      try {
+        // Navigate to the WaitingRoomScreen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => WaitingRoomScreen(
+              quiz: Quiz(
+                id: roomValidationService.roomId!, // Pass the room ID to the waiting room.
+                title: 'Nothing', // Provide a sample title.
+                description: 'Nothing', // Provide a sample description.
+                duration: 0, // Provide a sample duration.
+                questions: [], // Provide an empty list of questions.
+              ),
+              username: roomValidationService.username!, // Pass the username to the waiting room.
+              isHost: false, // This user is not the host.
+            ),
+          ),
+        );
+      } catch (e) {
+        print('Failed to join room: $e');
+      }
     } else {
       print('Room ID is null from roomValidationService.roomId');
     }
-    roomValidationService.isActive = false;
-    widget.validationDone(roomValidationService.isActive);
   }
 
   @override
   Widget build(BuildContext context) {
     final gameListService = Provider.of<GameListService>(context);
 
-    return FutureBuilder<void>(
-      future: _initializeFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else {
-          return StreamBuilder<List<GameListItem>>(
-            stream: gameListService.games$,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Center(child: Text('No active games.'));
-              } else {
-                final games = snapshot.data!;
-                return ListView.builder(
-                  itemCount: games.length,
-                  itemBuilder: (context, index) {
-                    final game = games[index];
-                    return ListTile(
-                      title: Text(_getQuizName(game.quizId)),
-                      subtitle: Text('Prestige: ${_minimumPrestige(game.prestige)}'),
-                      onTap: () => _joinRoom(game),
-                    );
-                  },
-                );
-              }
-            },
-          );
-        }
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Active Games'),
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            print('Error: ${snapshot.error}');
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return StreamBuilder<List<GameListItem>>(
+              stream: gameListService.games$,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  print('Error: ${snapshot.error}');
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No active games.'));
+                } else {
+                  final games = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: games.length,
+                    itemBuilder: (context, index) {
+                      final game = games[index];
+                      return ListTile(
+                        title: Text(_getQuizName(game.quizId)),
+                        subtitle: Text('Prestige: ${_minimumPrestige(game.prestige)}'),
+                        onTap: () => {
+                          _joinRoom(game),
+                          },
+                      );
+                    },
+                  );
+                }
+              },
+            );
+          }
+        },
+      ),
     );
   }
 
