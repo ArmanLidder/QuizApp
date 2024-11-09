56a9ebd7956aa8b8f41d186baa41c3f4eaf8a4fc
Mehdi <mehdi.el-harami@polymtl.ca>
2024-11-09 22:07:30 +0100

    fixed multiple bugs

--- projet/client_leger/polyquiz/lib/widgets/game_widgets/host_widgets/host_interface_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/game_widgets/host_widgets/host_interface_widget.dart
@@ -10,9 +10,15 @@ import 'package:polyquiz/widgets/game_widgets/question_info_widget.dart';
 import 'package:polyquiz/widgets/game_widgets/quit_btn.dart';
 import 'package:polyquiz/widgets/game_widgets/timer_widget.dart';
 import 'package:polyquiz/widgets/game_widgets/host_widgets/histogram_legend_widget.dart';
+import 'package:polyquiz/services/game_interface_management_service.dart';
+import 'package:polyquiz/services/interactive_list_service.dart';
 
 class HostInterface extends StatefulWidget {
-  HostInterface({super.key});
+  final InteractiveListService? interactiveListService;
+  final GameInterfaceManagementService? gameInterfaceManagementService;
+
+  HostInterface({Key? key, this.interactiveListService, this.gameInterfaceManagementService})
+      : super(key: key);
 
   @override
   State<HostInterface> createState() => _HostInterfaceState();
@@ -30,9 +36,29 @@ class _HostInterfaceState extends State<HostInterface> {
   @override
   void initState() {
     super.initState();
+    print('isSocketAlive: ${_socketService.isSocketAlive()}');
+    print('isAlreadyInit host interface: ${hostInterfaceManagementService.isAlreadyInit}');
     if (_socketService.isSocketAlive() && !hostInterfaceManagementService.isAlreadyInit) {
       hostInterfaceManagementService.configureBaseSocketFeatures(context);
       gameService.init(gameService.realGameService.roomId.toString());
+    }
+  }
+
+  void _handleLastQuestion() {
+    setState(() {
+      isResultPage = true;
+      isLastButton = true;
+    });
+    hostInterfaceManagementService.handleLastQuestion();
+  }
+
+  void _handleNextQuestion() {
+    hostInterfaceManagementService.saveStats();
+    if (gameService.realGameService.isLast) {
+      _handleLastQuestion();
+    } else {
+      gameService.realGameService.isNotified = false;
+      hostInterfaceManagementService.requestNextQuestion();
     }
   }
 
@@ -54,15 +80,22 @@ class _HostInterfaceState extends State<HostInterface> {
               ),
             );
           } else if (isResultPage) {
-            return ResultPage();
+            return ResultPage(
+              gameService: gameService,
+              hostInterfaceManagementService: hostInterfaceManagementService,
+              interactiveListService: widget.interactiveListService,
+              gameInterfaceManagementService: widget.gameInterfaceManagementService,
+            );
           } else {
             return Column(
               children: [
                 HostHeader(
-                  isLastButton: isLastButton,
-                  isResultPage: isResultPage,
+                  isLastButton: gameService.realGameService.isLast,
+                  onNextQuestion: _handleNextQuestion,
                   gameService: gameService,
                   hostInterfaceManagementService: hostInterfaceManagementService,
+                  interactiveListService: widget.interactiveListService,
+                  gameInterfaceManagementService: widget.gameInterfaceManagementService,
                 ),
                 HostMiddleSection(
                   gameService: gameService,
@@ -80,15 +113,19 @@ class _HostInterfaceState extends State<HostInterface> {
 
 class HostHeader extends StatelessWidget {
   bool isLastButton;
-  bool isResultPage;
+  final VoidCallback onNextQuestion;
   final GameService gameService;
   final HostInterfaceManagementService hostInterfaceManagementService;
+  final InteractiveListService? interactiveListService;
+  final GameInterfaceManagementService? gameInterfaceManagementService;
 
   HostHeader({
     required this.isLastButton,
-    required this.isResultPage,
+    required this.onNextQuestion,
     required this.gameService,
     required this.hostInterfaceManagementService,
+    this.interactiveListService,
+    this.gameInterfaceManagementService,
   });
 
   @override
@@ -108,22 +145,27 @@ class HostHeader extends StatelessWidget {
           questionText: gameService.question!.text,
         ),
         TextButton(
-          onPressed: () {
-            hostInterfaceManagementService.saveStats();
-            if (isLastButton) {
-              isResultPage = true;
-            }
-            if (gameService.realGameService.isLast) {
-              gameService.realGameService.isNotified = false;
-              hostInterfaceManagementService.handleLastQuestion();
-              isLastButton = true;
-            } else {
-              gameService.realGameService.isNotified = false;
-              hostInterfaceManagementService.requestNextQuestion();
-            }
-          },
+          onPressed: onNextQuestion,
+          // () 
+          // {
+            // hostInterfaceManagementService.saveStats();
+            // if (gameService.realGameService.isLast) {
+            //   isResultPage = true;
+            // }
+            // if (gameService.realGameService.isLast) {
+            //   print('isLast');
+            //   gameService.realGameService.isNotified = false;
+            //   hostInterfaceManagementService.handleLastQuestion();
+            //   isLastButton = true;
+            // } 
+            // else {
+            //   gameService.realGameService.isNotified = false;
+            //   hostInterfaceManagementService.requestNextQuestion();
+            // }
+            
+          // },
           child: Text(
-            isLastButton ? 'Dernière question' : 'Prochaine question',
+            gameService.realGameService.isLast ? 'Dernière question' : 'Prochaine question',
             style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1), fontSize: 20),
           ),
           style: TextButton.styleFrom(
@@ -131,7 +173,7 @@ class HostHeader extends StatelessWidget {
             backgroundColor: Color.fromRGBO(53, 121, 246, 1),
           ),
         ),
-        QuitBtn(isHost: true, roomId: gameService.realGameService.roomId),
+        QuitBtn(isHost: true, roomId: gameService.realGameService.roomId, gameService: gameService, interactiveListService: interactiveListService, gameInterfaceManagementService: gameInterfaceManagementService,hostInterfaceManagementService: hostInterfaceManagementService),
       ],
     );
   }
@@ -169,13 +211,36 @@ class HostMiddleSection extends StatelessWidget {
 }
 
 class ResultPage extends StatelessWidget {
+  final GameService gameService;
+  final InteractiveListService? interactiveListService;
+  final GameInterfaceManagementService? gameInterfaceManagementService;
+  final HostInterfaceManagementService hostInterfaceManagementService;
+
+  ResultPage({
+    required this.gameService,
+    this.interactiveListService,
+    this.gameInterfaceManagementService,
+    required this.hostInterfaceManagementService,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Game Over! Here are the results.',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Game Over! Here are the results.',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        QuitBtn(
+          isHost: true,
+          roomId: gameService.realGameService.roomId,
+          gameService: gameService,
+          interactiveListService: interactiveListService,
+          gameInterfaceManagementService: gameInterfaceManagementService,
+          hostInterfaceManagementService: hostInterfaceManagementService,
+        ),
+      ],
     );
   }
 }
