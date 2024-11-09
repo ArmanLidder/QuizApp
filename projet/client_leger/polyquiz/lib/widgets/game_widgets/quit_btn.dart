74f91f1dc4233a1121943cd69fb7a9cc7b45d205
Mehdi <mehdi.el-harami@polymtl.ca>
2024-11-09 22:08:07 +0100

    added reset game after quit

--- projet/client_leger/polyquiz/lib/widgets/game_widgets/quit_btn.dart
+++ projet/client_leger/polyquiz/lib/widgets/game_widgets/quit_btn.dart
@@ -2,15 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:polyquiz/constants/socket-event.dart';
 import 'package:polyquiz/services/global_navigation_service.dart';
 import 'package:polyquiz/services/socket_service.dart';
+import 'package:polyquiz/services/host_interface_management_service.dart';
+import 'package:polyquiz/services/game_service.dart';
+import 'package:polyquiz/services/game_interface_management_service.dart';
+import 'package:polyquiz/services/interactive_list_service.dart';
+import 'package:polyquiz/services/host_interface_management_service.dart';
 
 class QuitBtn extends StatelessWidget {
   final bool isHost;
   final int roomId;
+  final GameService? gameService;
+  final InteractiveListService? interactiveListService;
+  final GameInterfaceManagementService? gameInterfaceManagementService;
+  final HostInterfaceManagementService? hostInterfaceManagementService;
   final GlobalNavigationService _globalNavigationService =
       GlobalNavigationService();
   final SocketService _socketService = SocketService();
 
-  QuitBtn({Key? key, required this.isHost, required this.roomId})
+  QuitBtn({Key? key, required this.isHost, required this.roomId, this.gameService, this.interactiveListService, this.gameInterfaceManagementService, this.hostInterfaceManagementService})
       : super(key: key);
 
   @override
@@ -20,6 +29,18 @@ class QuitBtn extends StatelessWidget {
         if (this.isHost) {
           this._socketService.sendMessage(SocketEvent.HOST_LEFT, roomId);
           print('HOST LEFT');
+          if (gameService != null) {
+            gameService!.destroy();
+          }
+          if (gameInterfaceManagementService != null) { 
+            gameInterfaceManagementService!.reset();
+          }
+          if (interactiveListService != null){
+            interactiveListService!.reset();
+          }
+          if (hostInterfaceManagementService != null) {
+            this.hostInterfaceManagementService!.isAlreadyInit = false;
+          }
         } else {
           this._socketService.sendMessage(SocketEvent.PLAYER_LEFT, roomId);
           print('PLAYER LEFT');
