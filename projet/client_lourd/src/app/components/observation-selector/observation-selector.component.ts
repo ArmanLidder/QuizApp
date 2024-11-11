0c8211158eb0eacba31820818aaa7beed7ade848
hb.96.montreal <hb.96.montreal@gmail.com>
2024-11-10 20:39:13 -0500

    feature: New Component Observation selector
    - Component which enables observer to see current selected player and choose other player

---
+++ projet/client_lourd/src/app/components/observation-selector/observation-selector.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {ObservationService} from "@app/services/observation.service/observation.service";
+import {
+    ObservationSelectorDialogComponent
+} from "@app/components/observation-selector-dialog/observation-selector-dialog.component";
+
+@Component({
+    selector: 'app-observation-selector',
+    templateUrl: './observation-selector.component.html',
+    styleUrls: ['./observation-selector.component.scss']
+})
+export class ObservationSelectorComponent implements OnInit {
+    isMenuOpen: boolean = false;
+
+    constructor(
+        private dialog: MatDialog,
+        public observationService: ObservationService,
+    ) {
+        console.log("ON construct ");
+    }
+
+    ngOnInit() {
+        console.log("ON FCK INIT");
+        // this.playerObserved = this.observationService.gameConfigs.hostUserId;
+    }
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
+
+    openSelectorDialog() {
+        this.toggleMenu();
+        const dialogRef = this.dialog.open(ObservationSelectorDialogComponent, {
+            data: {playerObserved: this.observationService.observedPlayerId}
+        }).afterClosed();
+        dialogRef.subscribe(result => {
+            if (result) {
+                console.log(`${result}`);
+                this.observationService.observedPlayerId = result;
+            } else {
+                console.log("Dialog was closed without selecting a user.");
+            }
+        });
+    }
+}
