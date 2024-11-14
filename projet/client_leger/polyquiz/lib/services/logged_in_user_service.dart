3476b69d9da42c989d4f43118f22be0f585b10f2
PageotM <maxime.pageot.313@gmail.com>
2024-11-14 16:07:46 -0500

    removed flag that allows to login even if isConnected is true

--- projet/client_leger/polyquiz/lib/services/logged_in_user_service.dart
+++ projet/client_leger/polyquiz/lib/services/logged_in_user_service.dart
@@ -57,7 +57,7 @@ class LoggedInUserService extends GetxController {
     // Check if the user was successfully set
     if (currentUser != null) {
       // Update the `isOnline` status in Firebase
-      if (false && this.user!.isConnected){ //TODO: disable this false flag
+      if (this.user!.isConnected){ //TODO: disable this false flag
         throw("USER ALREADY CONNECTED");
       }
       await FirebaseFirestore.instance
