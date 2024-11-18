0744f6cc0bdfbfbf5beb5e385f1fae932e3a4722
hb.96.montreal <hb.96.montreal@gmail.com>
2024-11-18 14:14:26 -0500

    Fix: Observateur-counter reset
    - Bug: when socket get disconnect it does not add the handling Obs Count envent
    
    - add it on init observateur-counter

--- projet/client_lourd/src/app/components/observer-counter/observer-counter.component.ts
+++ projet/client_lourd/src/app/components/observer-counter/observer-counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {ObserverCounterService} from "@app/services/observer-counter.service/observer-counter.service";
 
 @Component({
@@ -6,7 +6,13 @@ import {ObserverCounterService} from "@app/services/observer-counter.service/obs
   templateUrl: './observer-counter.component.html',
   styleUrls: ['./observer-counter.component.scss']
 })
-export class ObserverCounterComponent {
+export class ObserverCounterComponent implements OnInit {
 
   constructor(public observerCounterService: ObserverCounterService) {}
+
+  ngOnInit() {
+    console.log("ngOnit Observer Count Service");
+    this.observerCounterService.initialize();
+  }
+
 }
