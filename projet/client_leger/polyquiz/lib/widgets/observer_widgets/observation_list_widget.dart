9cef71ef352ba13e8d1858d8ae212a81f9f01536
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-11-22 04:41:25 -0500

    made rudimentary observation list widget

--- projet/client_leger/polyquiz/lib/widgets/observer_widgets/observation_list_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/observer_widgets/observation_list_widget.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:polyquiz/services/observation_service.dart';
+import 'package:polyquiz/widgets/user_widget/smartAvatar.dart';
 
 class ObservationListWidget extends StatelessWidget {
-  const ObservationListWidget({super.key});
+  ObservationListWidget({super.key});
+
+  // LES VALEURS DE TEXTES
+  final titleText = "Choisir un joueur à observer";
+  final observeButtonText = "Observer";
+  final cancelButtonText = "Annuler";
+
+  // SERVICES
+  final observationService = ObservationService.instance;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          getTitleWidget(),
+          getPlayerListWidget(),
+          getCancelButton()
+        ]
+      ),
+    );
+  }
+
+  Widget getTitleWidget() {
+    return Center(
+      child: Text(titleText),
+    );
+  }
+
+  Widget getPlayerListWidget() {
+    final playerList = observationService.playerList;
+    return ListView.builder(
+        itemCount: playerList.length,
+        itemBuilder: (BuildContext context, int index) => getPlayerTile(playerList[index])
+    );
+  }
+  
+  Widget getPlayerTile(String uid) {
+    Widget observeButton = TextButton(
+        onPressed: () { observationService.observeOtherPlayer(uid); },
+        child: Text(observeButtonText)
+    );
+
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            SmartAvatar(userId: uid, interactible: false, hasName: true,),
+            observeButton
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getCancelButton() {
+    return Center(
+      child: TextButton(onPressed: (){}, child: Text(cancelButtonText)),
+    );
   }
 }
