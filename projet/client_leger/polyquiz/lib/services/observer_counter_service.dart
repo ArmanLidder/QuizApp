9c36523b14d9bc65e6b2e3c0451a0f190e37c7d2
Mehdi <mehdi.el-harami@polymtl.ca>
2024-11-22 19:52:34 +0100

    added observationcounterservice

---
+++ projet/client_leger/polyquiz/lib/services/observer_counter_service.dart
@@ -0,0 +1,27 @@
+import 'package:polyquiz/services/socket_service.dart';
+import 'package:polyquiz/constants/socket-event.dart';
+import 'game_service.dart';
+import 'package:flutter/material.dart';
+
+class ObserverCounterService extends ChangeNotifier {
+  SocketService socketService = SocketService();
+  GameService gameService = GameService();
+  int obsCount = 0;
+
+
+  void initialize() {
+    socketService.sendMessage(SocketEvent.GET_OBS_COUNT, gameService.realGameService.roomId);
+
+    if (socketService.isSocketAlive()) {
+      handleUpdateCounter();
+    }
+  }
+
+  void handleUpdateCounter() {
+    socketService.onMessage(SocketEvent.UPDATE_OBS_COUNT, (count) {
+      obsCount = count;
+      notifyListeners();
+      print('ObserverCounter initialized ${obsCount}');
+    });
+  }
+}
