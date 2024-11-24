f185ef06ca083da50ff4ace2c28566a448c7fa3a
ArmanLidder <arman.lidder@polymtl.ca>
2024-11-23 21:03:46 -0500

    Fix : Do not show chat on register page and login page ever

--- projet/client_lourd/src/app/pages/app/app.component.ts
+++ projet/client_lourd/src/app/pages/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnDestroy, HostListener} from '@angular/core';
-import {SocketClientService} from "@app/services/socket-client.service/socket-client.service";
-import {AuthService} from "@app/services/auth.service/auth.service";
-import {UsersService} from "@app/services/users.service/users.service";
-import {UserSettingsService} from "@app/services/user-settings.service/user-settings.service";
-
+import { Component, OnDestroy, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocketClientService } from "@app/services/socket-client.service/socket-client.service";
+import { AuthService } from "@app/services/auth.service/auth.service";
+import { UsersService } from "@app/services/users.service/users.service";
+import { UserSettingsService } from "@app/services/user-settings.service/user-settings.service";
 
 @Component({
     selector: 'app-root',
@@ -11,17 +11,25 @@ import {UserSettingsService} from "@app/services/user-settings.service/user-sett
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
+    showChat: boolean = true;
 
-    constructor(private socketService: SocketClientService,
-                private authService: AuthService,
-                public usersService: UsersService,
-                public settings: UserSettingsService) {}
-
+    constructor(
+        private socketService: SocketClientService,
+        private authService: AuthService,
+        public usersService: UsersService,
+        public settings: UserSettingsService,
+        private router: Router
+    ) {
+        this.router.events.subscribe(() => {
+            const currentRoute = this.router.url;
+            this.showChat = !(currentRoute.includes('/login') || currentRoute.includes('/register'));
+        });
+    }
 
     @HostListener('window:beforeunload')
-    removeToken() {
+    async removeToken() {
         if (this.socketService.isSocketAlive()) this.socketService.disconnect();
-        this.authService.logout();
+        await this.authService.logout();
     }
 
     async ngOnDestroy() {
