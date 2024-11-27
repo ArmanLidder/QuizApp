d8424edba7af4a4f7f90cbf94d08559127eb84a0
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-11-27 12:50:37 -0500

    FIX: QCM answer not persisting after validation

--- projet/client_leger/polyquiz/lib/widgets/game_widgets/player_widgets/player_qcm_choice_widget.dart
+++ projet/client_leger/polyquiz/lib/widgets/game_widgets/player_widgets/player_qcm_choice_widget.dart
@@ -54,12 +54,15 @@ class _PlayerQcmChoiceWidgetState extends State<PlayerQcmChoice> {
   @override
   Widget build(BuildContext context) {
     print("Rebuilding button ${widget.index}");
-    if (this.gameInterfaceManagementService.gameService.answers.containsKey(widget.index)) {
-      textBtnColor = Color.fromRGBO(53, 121, 246, 1);
-      print('Button ${widget.index} is TRUE');
-    } else {
-      textBtnColor = Color.fromRGBO(0, 0, 0, 0);
-      print('Button ${widget.index} is FALSE');
+    if (this.gameInterfaceManagementService.gameService.isObserverMode) {
+      if (this.gameInterfaceManagementService.gameService.answers.containsKey(
+          widget.index)) {
+        textBtnColor = Color.fromRGBO(53, 121, 246, 1);
+        print('Button ${widget.index} is TRUE');
+      } else {
+        textBtnColor = Color.fromRGBO(0, 0, 0, 0);
+        print('Button ${widget.index} is FALSE');
+      }
     }
     print('after rebuilding buttonn, answers are ${this.gameInterfaceManagementService.gameService.answers}');
     if (gameInterfaceManagementService.getQcmEnabled()) {
