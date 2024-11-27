2583d509aaf834072d48d1807f4b0b77ad57443b
PageotM <maxime.pageot.313@gmail.com>
2024-11-27 16:00:23 -0500

    gameView couleur dynamique

--- projet/client_leger/polyquiz/lib/pages/game_page.dart
+++ projet/client_leger/polyquiz/lib/pages/game_page.dart
@@ -160,11 +160,11 @@ class _MyWidgetState extends State<GamePage> {
         );
       });
     } else {
-      return Container(
+      return Obx(()=>Container(
           color: themeService.mainBackground.value,
           child: AnimatedBuilder(
               animation:
-                  _gameInterfaceManagementService.gameService.realGameService,
+              _gameInterfaceManagementService.gameService.realGameService,
               builder: (BuildContext context, Widget? snapshot) {
                 if (_gameInterfaceManagementService.gameService.question ==
                     null) {
@@ -197,9 +197,9 @@ class _MyWidgetState extends State<GamePage> {
                                   gameService: _gameInterfaceManagementService
                                       .gameService,
                                   interactiveListService:
-                                      _interactiveListService,
+                                  _interactiveListService,
                                   gameInterfaceManagementService:
-                                      _gameInterfaceManagementService,
+                                  _gameInterfaceManagementService,
                                 ),
                               ],
                             ),
@@ -221,26 +221,26 @@ class _MyWidgetState extends State<GamePage> {
                                             child: TimerWidget(
                                               isHost: isHost,
                                               timeTxt:
-                                                  _gameInterfaceManagementService
-                                                      .timerText,
+                                              _gameInterfaceManagementService
+                                                  .timerText,
                                               time:
-                                                  _gameInterfaceManagementService
-                                                      .gameService.timer,
+                                              _gameInterfaceManagementService
+                                                  .gameService.timer,
                                             ),
                                           ),
                                           Positioned.fill(
                                             child: Align(
                                               alignment: Alignment.center,
                                               child: QuestionInfoWidget(
-                                              questionNum:
+                                                  questionNum:
                                                   _gameInterfaceManagementService
                                                       .gameService
                                                       .questionNumber,
-                                              questionPts:
+                                                  questionPts:
                                                   _gameInterfaceManagementService
                                                       .gameService
                                                       .question?.points ?? 0,
-                                              questionText:
+                                                  questionText:
                                                   _gameInterfaceManagementService
                                                       .gameService
                                                       .question?.text ?? ''),
@@ -265,14 +265,14 @@ class _MyWidgetState extends State<GamePage> {
                                                             .mainAccent.value),
                                                   ),
                                                   _gameInterfaceManagementService
-                                                          .isBonus
+                                                      .isBonus
                                                       ? Text(
-                                                          gameText[
-                                                              'BONUS_RECEIVED_FEEDBACK'],
-                                                          style: TextStyle(
-                                                              color: themeService
-                                                                  .mainAccent
-                                                                  .value))
+                                                      gameText[
+                                                      'BONUS_RECEIVED_FEEDBACK'],
+                                                      style: TextStyle(
+                                                          color: themeService
+                                                              .mainAccent
+                                                              .value))
                                                       : SizedBox()
                                                 ],
                                               ),
@@ -291,12 +291,12 @@ class _MyWidgetState extends State<GamePage> {
                                         child: PlayerNotice(
                                           message: message,
                                           gameInterfaceManagementService:
-                                              _gameInterfaceManagementService,
+                                          _gameInterfaceManagementService,
                                         ),
                                       ),
                                       Row(
                                         mainAxisAlignment:
-                                            MainAxisAlignment.center,
+                                        MainAxisAlignment.center,
                                         children: [getButtons()],
                                       )
                                     ],
@@ -310,7 +310,7 @@ class _MyWidgetState extends State<GamePage> {
                         }
                       });
                 }
-              }));
+              })));
     }
   }
 
