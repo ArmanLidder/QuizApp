82a88bf38f38ac5fb4719453a7b9365bfa4df589
Mehdi <mehdi.el-harami@polymtl.ca>
2024-11-28 00:54:26 +0100

    remove

--- projet/client_leger/polyquiz/lib/constants/text/FrenchTextValue.dart
+++ projet/client_leger/polyquiz/lib/constants/text/FrenchTextValue.dart
@@ -533,8 +533,7 @@ const frenchTextValues = {
       "TIME_LEFT": "Temps restant ",
       "FINAL_RESULT": "Résultats finaux \n s'afficherons dans ",
       "RESULT_AVAILABLE_IN": "Résultat dans ",
-      "NEXT": "Prochaine \n question dans ",
-      "LOADING_QUESTIONS": "Chargement des questions...",
+      "NEXT": "Prochaine \n question dans "
     },
     "QRE_HISTOGRAM_X_VAL": {
       "WITHIN_MARGIN": "Dans l'intervalle",
