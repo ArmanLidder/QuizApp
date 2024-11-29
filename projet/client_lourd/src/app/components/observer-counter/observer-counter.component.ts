9c582d57f8a6d03e116696c55366afcca9c31e9a
hb.96.montreal <hb.96.montreal@gmail.com>
2024-11-29 03:22:32 -0500

    Remove Ng0201 warning by lazy injecting

--- projet/client_lourd/src/app/components/observer-counter/observer-counter.component.ts
+++ projet/client_lourd/src/app/components/observer-counter/observer-counter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ObserverCounterService} from "@app/services/observer-counter.service/observer-counter.service";
+import {GameService} from "@app/services/game.service/game.service";
 
 @Component({
   selector: 'app-observer-counter',
@@ -8,11 +9,11 @@ import {ObserverCounterService} from "@app/services/observer-counter.service/obs
 })
 export class ObserverCounterComponent implements OnInit {
 
-  constructor(public observerCounterService: ObserverCounterService) {}
+  constructor(public observerCounterService: ObserverCounterService, private gameService: GameService) {}
 
   ngOnInit() {
     console.log("ngOnit Observer Count Service");
-    this.observerCounterService.initialize();
+    this.observerCounterService.initialize(this.gameService);
   }
 
 }
