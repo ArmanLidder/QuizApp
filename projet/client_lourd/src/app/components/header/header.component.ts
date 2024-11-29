6f3b19f4aea2f9210d7e870ce5c848a9a5531c78
ArmanLidder <arman.lidder@polymtl.ca>
2024-11-29 13:16:39 -0500

    Header component logo redirects to home page
    In case of emergency

--- projet/client_lourd/src/app/components/header/header.component.ts
+++ projet/client_lourd/src/app/components/header/header.component.ts
@@ -7,6 +7,13 @@ import {Observable} from "rxjs";
 import {User} from "@common/interfaces/user-data.interface";
 import {UserSettingsService} from "@app/services/user-settings.service/user-settings.service";
 import {SettingsDialogComponent} from "@app/components/settings-dialog/settings-dialog.component";
+import {Router} from "@angular/router";
+import {ConfirmationDialogComponent} from "@app/components/confirmation-dialog/confirmation-dialog.component";
+import {TranslateService} from "@ngx-translate/core";
+import {PopUpMessage} from "@common/browser-message/displayable-message/pop-up-message";
+import {SocketEvent} from "@common/socket-event-name/socket-event-name";
+import {GameService} from "@app/services/game.service/game.service";
+import {SocketClientService} from "@app/services/socket-client.service/socket-client.service";
 
 
 @Component({
@@ -16,11 +23,17 @@ export class HeaderComponent implements OnInit {
     @Input() showMenu = true;
     @Input() observerMode = false;
     @Input() normalPlayer = false;
+    @Input() showWarningPopUpOnLeave = false;
+
     currentUser$: Observable<User | null>
     pendingRequests$: Observable<User[]>;
     currentLanguage: 'fr' | 'en';
 
-    constructor(public usersService: UsersService, private dialog: MatDialog, public friendsService: FriendService, private settings: UserSettingsService) {
+    constructor(public usersService: UsersService,
+                private dialog: MatDialog,
+                public friendsService: FriendService, private settings: UserSettingsService, private router: Router, private translate: TranslateService,
+                private gameService: GameService,
+                private socketService: SocketClientService,) {
     }
 
     ngOnInit() {
@@ -44,5 +57,25 @@ export class HeaderComponent implements OnInit {
             height: '250px',
             panelClass: 'settingsDialogClass'
         });
+    }
+    async navigateToHome() {
+        if (this.showWarningPopUpOnLeave) {
+            const message = await this.translate.get(PopUpMessage.LEAVE_MESSAGE).toPromise();
+            const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+                width: '300px',
+                data: { message },
+            });
+            dialogRef.afterClosed().subscribe((result) => {
+                if (result) {
+                    const observerMode = this.gameService.observerMode;
+                    if (observerMode)
+                        this.socketService.send(SocketEvent.OBS_LEFT, {roomId: this.gameService.gameRealService.roomId, observedId: this.gameService.observedPlayerId});
+                    this.router.navigate(['/home']);
+                }
+            });
+        }
+        else if (this.showMenu) {
+            this.router.navigate(['/home']);
+        }
     }
 }
