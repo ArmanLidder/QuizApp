f2a73168b91b80238a36558cfec3482cb53d0ce7
Arman Lidder <arman.lidder@polymtl.ca>
2026-05-22 16:02:54 -0400

    Delete prototype directory

--- prototype/client_leger/polyquiz/android/app/src/main/kotlin/com/example/polyquiz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.polyquiz.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- prototype/server/src/config/config.ts
+++
@@ -1,4 +0,0 @@
-// Not good practice but ok for prototype
-
-export const jwtSecret = 'your_jwt_secret_key';
-export const mongoURI = 'mongodb+srv://hb96montreal:A8l7Bu5h9IiR4hVx@cluster.8vblv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster';
